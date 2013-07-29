03058de9bf62180c3c4bb1e60a187c00e48373ad
Yannick Schutz <yannick.schutz@gmail.com>
2013-07-29 16:47:03 +0200

    Handle error codes in Twitter::ClientError

--- lib/twitter/error/client_error.rb
+++ lib/twitter/error/client_error.rb
@@ -5,27 +5,41 @@ module Twitter
     # Raised when Twitter returns a 4xx HTTP status code or there's an error in Faraday
     class ClientError < Twitter::Error
 
+      attr_reader :code
+
       # Create a new error from an HTTP environment
       #
       # @param response [Hash]
       # @return [Twitter::Error]
       def self.from_response(response={})
-        new(parse_error(response[:body]), response[:response_headers])
+        error, code = parse_error(response[:body])
+        new(error, response[:response_headers], code)
+      end
+
+      # Initializes a new ClientError object
+      #
+      # @param exception [Exception, String]
+      # @param response_headers [Hash]
+      # @param code [Integer]
+      # @return [Twitter::ClientError]
+      def initialize(exception=$!, response_headers={}, code=nil)
+        @code = code
+        super(exception, response_headers)
       end
 
     private
 
       def self.parse_error(body)
         if body.nil?
-          ''
+          ['', nil]
         elsif body[:error]
-          body[:error]
+          [body[:error], nil]
         elsif body[:errors]
           first = Array(body[:errors]).first
           if first.is_a?(Hash)
-            first[:message].chomp
+            [first[:message].chomp, first[:code]]
           else
-            first.chomp
+            [first.chomp, nil]
           end
         end
       end
--- spec/twitter/error/client_error_spec.rb
+++ spec/twitter/error/client_error_spec.rb
@@ -18,6 +18,27 @@ describe Twitter::Error::ClientError do
         end
       end
     end
+    context "when HTTP status is #{status} and body is errors" do
+      context "when errors is an array of hashes" do
+        [nil, 187].each do |error_code|
+          context "when error coder is #{error_code.inspect}" do
+            before do
+              if error_code.nil?
+                body_message = '{"errors":[{"message":"Client Error"}]}'
+              else
+                body_message = '{"errors":[{"message":"Client Error","code": ' + error_code.to_s + ' }]}'
+              end
+              stub_get("/1.1/statuses/user_timeline.json").with(:query => {:screen_name => "sferik"}).to_return(:status => status, :body => body_message)
+            end
+            it "raises #{exception.name}" do
+              expect{@client.user_timeline("sferik")}.to raise_error { |error|
+                expect(error.code).to eq error_code
+              }
+            end
+          end
+        end
+      end
+    end
   end
 
 end
